1547f6e1efb59c1a9c3448b22f872e4c075bb32d
Akira Masuda <masuda.akira1014@gmail.com>
2022-08-22 00:38:04 +0900

    finished creating replacer

--- README.md
+++ README.md
@@ -6,20 +6,29 @@ Kubernetes kustomization manifests run on my home k8s.
 
 Before this repo's manifest automatically synced, we need to do things below.
 
-- ArgoCD deployed on k8s
+- [ArgoCD](https://argo-cd.readthedocs.io/en/stable/) deployed on k8s
 - Setup vault
+
+### Setup vault
+
+Private configuration is required to generate vault manifst, so we execute secret replace tool to generate manifest.
 
 ```bash
 kubectl apply -f ./base/vault/vault-server.yaml
 # need to change variable according to your environment
-kubectl apply -f ./base/vault/vault-persistent-volume.yaml
-kubectl apply -f ./base/argocd/argocd-vault-plugin-secret.yaml
+cp sample.envrc .envrc
+vim .envrc
+source .envrc
+go run tools/cmd/replace.go ./base/vault/vault-persistent-volume.yaml | kubectl apply --dry-run='server' -f -
+go run tools/cmd/replace.go ./base/argocd/argocd-vault-plugin-secret.yaml | kubectl apply --dry-run='server' -f -
 # also set the same secret value to deployed vault so argocd-vault-plugin can complement the values
 vault write /home-k8s/vault/persistent-volume-nfs-ip 192.168.x.x
 vault write /home-k8s/vault/persistent-volume-nfs-volume-path /volume1/xxx/xxx
 vault write /home-k8s/argocd-vault-plugin/vault-address 192.168.x.x 
 vault write /home-k8s/argocd-vault-plugin/vault-token xxxxxx
 ```
+
+After a vault started on k8s and argocd-vault-plugin is setup, private data are read from vault.
 
 ## Setup ArgoCD and sync
 
--- tools/cmd/replace.go
+++ tools/cmd/replace.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -18,48 +19,61 @@ func convertToEnvVarStr(s string) string {
 	return ev
 }
 
-func replaceAVPPlaceholder(filePathStr string) error {
+func replaceAVPPlaceholder(filePathStr string) (string, error) {
 	var fi os.FileInfo
 	var err error
 	if fi, err = os.Stat(filePathStr); err != nil {
-		return fmt.Errorf("could not find path %s: %w", filePathStr, err)
+		return "", fmt.Errorf("could not find path %s: %w", filePathStr, err)
 	}
 	if fi.IsDir() {
-		return fmt.Errorf("given path is dir: %s", filePathStr)
+		return "", fmt.Errorf("given path is dir: %s", filePathStr)
 	}
 	var fp *os.File
 	if fp, err = os.Open(filePathStr); err != nil {
-		return fmt.Errorf("open file: %w", err)
+		return "", fmt.Errorf("open file: %w", err)
 	}
 	defer fp.Close()
 
 	sc := bufio.NewScanner(fp)
+	var sb strings.Builder
 	for sc.Scan() {
 		line := sc.Text()
-		fmt.Println(line)
 		matches := placeholderRegex.FindAllStringSubmatch(line, -1)
 		if len(matches) == 0 {
+			sb.WriteString(line + "\n")
 			continue
 		}
+		replacedLine := line
 		for _, submatches := range matches {
 			if len(submatches) == 0 {
-				continue
+				return "", fmt.Errorf("could not find submatch: %v", matches)
 			}
 			submatch := submatches[1]
 			evs := convertToEnvVarStr(submatch)
-			fmt.Println(evs)
 			ev := os.Getenv(evs)
-			fmt.Println(ev)
+			if ev == "" {
+				return "", fmt.Errorf("failed to replace %s, could not find environment valiable %s", submatch, evs)
+			}
+			if _, err := strconv.ParseFloat(ev, 64); err == nil {
+				// is numeric
+				replacedLine = placeholderRegex.ReplaceAllString(replacedLine, ev)
+			} else {
+				// double quote string value
+				replacedLine = placeholderRegex.ReplaceAllString(replacedLine, `"`+ev+`"`)
+			}
 		}
+		sb.WriteString(replacedLine + "\n")
 	}
-	return nil
+	return sb.String(), nil
 }
 
 func main() {
 	if len(os.Args) == 1 {
-		panic("usage: replace <any_file_path>")
+		panic("usage: go run ./tools/cmd/replace <any_file_path>")
 	}
-	if err := replaceAVPPlaceholder(os.Args[1]); err != nil {
+	if val, err := replaceAVPPlaceholder(os.Args[1]); err != nil {
 		panic(fmt.Errorf("replace argo vault plugin placeholder failed: %w", err))
+	} else {
+		fmt.Println(val)
 	}
 }
